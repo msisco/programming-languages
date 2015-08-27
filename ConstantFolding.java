146390d90b760e95808fa4e3a504db408c0ce66d
Matt Sisco <msisco@zonoff.com>
2015-08-27 12:19:58 -0400

    Added Worksheet One answers

---
+++ ConstantFolding.java
@@ -0,0 +1,7 @@
+public class ConstantFolding{
+	
+	public static void main(String[] args){
+		int x = 3+4;
+		System.out.println(x);
+	}
+}
---
+++ ConstantPropogation.java
@@ -0,0 +1,8 @@
+public class ConstantPropogation {
+	public static void main(String[] args){
+		int x = 4;
+		int y = x +2;
+		System.out.println(x);
+		System.out.println(y);
+	}
+}
